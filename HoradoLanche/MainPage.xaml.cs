42d652ba853cc0a3864c772aac1c3e9f17ba6aa2
Amaral1973 <61523977+Amaral1973@users.noreply.github.com>
2021-01-21 20:02:59 -0300

    Tarefa
    Tarefa realizada, pasta Android Zipada.

---
+++ HoradoLanche/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HoradoLanche
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            bt.Text = "Mais Android!!!";
+            foto.Source = "android2";
+        }
+    }
+}
